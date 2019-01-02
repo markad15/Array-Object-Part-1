042ee6e16c646858f45826ef11b1115f9dce1442
Ankush <markad.ankush@yahoo.com>
2019-01-02 12:54:38 +0530

    Final Submit

--- externalfile.js
+++ externalfile.js
@@ -36,6 +36,7 @@ var empNames = new Array("Nikhil","Bhushan","Pooja");
 
 
 //4.Create Array of object(Object should be id property ) and loop through Array and find all ids. Use For Each loop
+//5. 5.	Create Array of object(Object should be id property ) and loop through Array and find all ids. Use Map function 
 
 var emp1Info = {empID:6030, empName:"Ankush Markad", basicSalary:45000};
 var emp2Info = {empID:5030, empName:"Nikhil Kalamkar", basicSalary:65000};
@@ -43,5 +44,38 @@ var empInformation = [emp1Info, emp2Info];
 
 function empDetailsFunction(empData, empIndex)
 {  
-    document.getElementById("empDataid").innerHTML = document.getElementById("empDataid").innerHTML + empInformation.empID;
+    document.getElementById("empDataid").innerHTML = document.getElementById("empDataid").innerHTML + "Employee Id:" + empData.empID + "<br/>";
+}
+
+
+//6. Find Angular in given array [“C#”, “Javascript”,”Angular”,”HTML”]
+
+function findLangInfo()
+{
+    var langInfo = ["C#", "Javascript","Angular","HTML"];
+    var angularIndex = langInfo.indexOf("Angular");
+    document.getElementById("langInfoId").innerHTML = angularIndex;
+}
+
+var langString = ["C#", "Javascript","Angular","HTML"];
+
+function arrayMethods()
+{ 
+    //7. Add New “Typescript ,AmaZon Web Service, SAP” in Array [“C#”, “Javascript”,”Angular”,”HTML”]
+   // langString.push("Typescript", "AmaZon Web Service", "SAP");
+   
+   //8.	Remove HTML from given Array [“C#”, “Javascript”,”Angular”,”HTML”]
+   //langString.pop();
+   
+   //9.	Remove “Javascript”  from  Array [“C#”, “Javascript”,”Angular”,”HTML”]
+   //langString.splice(1,1);
+
+   //document.getElementById("stringPid").innerHTML = langString;
+
+
+
+   //10. Get JAvascript and Angular from [“C#”, “Javascript”,”Angular”,”HTML”]
+
+    var newElmentString = langString.slice(1,3);
+    document.getElementById("stringPid").innerHTML = newElmentString;
 }
